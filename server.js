13b79cab49340d014dd2b6d8b5c31ba39f159599
Miguel <migang57@gmail.com>
2018-10-01 14:19:10 -0500

    deployment to heroku

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
-
-app.use(express.static(__dirname + './dist/house-power-frontend2'));
-app.listen(process.env.PORT || 8090);
-
-//PathLocationStrategy
-
-app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/house-power-frontend2/index.html'));
+ 
+const app = express();
+ 
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/house-power-frontend2'));
+ 
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/house-power-frontend2/index.html'));
 });
-console.log('Console listening!');
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
